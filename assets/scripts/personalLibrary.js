5acd760e79cac2aadbc91d06d4ea2ab48d188a49
Klea <you@example.com>
2023-11-17 22:17:49 +0100

    Remove Button

--- assets/scripts/personalLibrary.js
+++ assets/scripts/personalLibrary.js
@@ -17,7 +17,6 @@ function populate(){
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.category}</td>
-            <td>${book.image}</td>
             <td>
                 <button id="removeBtn" data-book-id="${book.id}">Remove</button>
             </td>
@@ -30,20 +29,20 @@ function populate(){
 
 populate();
 
-$("#removeBtn").click(function(){
-    let BooksToAdd = JSON.parse(localStorage.getItem('addedBooks')) || [];
-   
+
+
+
+$(booksTableBody).on('click', "#removeBtn", function(){
     const bookId = $(this).data('book-id');
     const book = BooksToAdd.find(n => n.id === bookId);
-    console.log('Found Book:', book);
+    // editOrder(order);
 
-    if (book && book.title) {
-    $("#remove-book").text(`${book.title}`);
+    localStorage.setItem('bookToBeRemovedId', bookId);
+
+
+    $("#remove-title").text(book.title);
+
     $("#removeModal").show();
-    }else {
-        console.log(`Book not found for ID: ${bookId}`);
-    }
-
 })
 
 $("#closeEditModalSpn").click(function(){
@@ -54,91 +53,29 @@ $("#cancelRemoveBtn").click(function(){
     $("#removeModal").hide();
 });
 
-$("#ConfirmBtn").click(function(){
-    var theRowId = $('#bookTbl tr').attr('data-book-id');
-    $('#booksTbl tr#'+theRowId).remove();
+$("#confirmBtn").click(function(){
+
+    //1. Get order id of the item to be removed
+    var bookId = localStorage.getItem('bookToBeRemovedId');
+
+    console.log('bookId from localstorage = ', bookId);
+
+    //2. Get from localstorage 
+    const index = BooksToAdd.findIndex(book => book.id == bookId);
+
+    console.log('all books = ', BooksToAdd);
+    console.log('index == ', index);
+
+
+    if(index !== -1){
+        BooksToAdd.splice(index,1);
+
+        localStorage.setItem('addedBooks', JSON.stringify(BooksToAdd))
+    } else {
+        console.log('Book not found')
+    }
+ 
     $("#removeModal").hide();
-});
+    location.reload();
 
-
-
- // Check if the clicked element is a "Remove" button
- /*if (target.classList.contains("removeBtn")) {
-    // Add functionality to remove the book
-    // For example, you can remove the entire row
-    target.closest('tr').remove();
-}*/
-
-/*
-// Function to add a new row to the table
-function addBookRow() {
-    var table = document.getElementById("ordersTbl").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.rows.length);
-
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-
-    var bookName = prompt("Enter the book name:");
-    var author = prompt("Enter the author:");
-    var description = prompt("Enter the description:");
-
-    cell1.innerHTML = bookName || "New Book";
-    cell2.innerHTML = author || "New Author";
-    cell3.innerHTML = description || "New Description";
-
-    var editBtn = document.createElement("button");
-    editBtn.innerHTML = "Edit";
-    editBtn.classList.add("editBtn");
-    cell4.appendChild(editBtn);
-
-    var removeBtn = document.createElement("button");
-    removeBtn.innerHTML = "Remove";
-    removeBtn.classList.add("removeBtn");
-    cell4.appendChild(removeBtn);
-}
-
-// Event listener for the "Add Book" button
-document.getElementById("addBtn").addEventListener("click", addBookRow);
-
-// Event delegation for dynamically added buttons
-document.getElementById("ordersTbl").addEventListener("click", function (event) {
-    var target = event.target;
-
-    // Check if the clicked element is an "Edit" button
-   if (target.classList.contains("editBtn")) {
-        // Get the index of the clicked button's cell
-        var columnIndex = target.parentElement.cellIndex;
-
-        // Ask the user which information to change
-        var changeType = prompt("Enter 'book' to change the book name, 'author' to change the author, or 'description' to change the description:");
-
-        if (changeType !== null) {
-            var newContent = prompt("Enter the new content:");
-
-            if (newContent !== null) {
-                switch (changeType.toLowerCase()) {
-                    case 'book':
-                        target.closest('tr').cells[0].innerHTML = newContent;
-                        break;
-                    case 'author':
-                        target.closest('tr').cells[1].innerHTML = newContent;
-                        break;
-                    case 'description':
-                        target.closest('tr').cells[2].innerHTML = newContent;
-                        break;
-                    default:
-                        alert("Invalid input. Please enter 'book', 'author', or 'description'.");
-                }
-            }
-        }
-    }
-
-    // Check if the clicked element is a "Remove" button
-    if (target.classList.contains("removeBtn")) {
-        // Add functionality to remove the book
-        // For example, you can remove the entire row
-        target.closest('tr').remove();
-    }
-    */
+})
